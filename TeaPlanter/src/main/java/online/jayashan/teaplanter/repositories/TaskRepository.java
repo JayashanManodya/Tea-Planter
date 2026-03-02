ffa7d5182d12042378f13c1d69f53a982d2bc394
Jayashan Manodya <kpjmp28@gmail.com>
2026-03-02 15:46:32 +0530

    delete some files

--- TeaPlanter/src/main/java/online/jayashan/teaplanter/repositories/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package online.jayashan.teaplanter.repositories;
-import online.jayashan.teaplanter.entities.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface TaskRepository extends JpaRepository<Task,Long> {
-    List<Task>findByWorkerId(Long workerId);
-    List<Task>findByStatus(String status);
-}
