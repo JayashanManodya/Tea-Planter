87b5f3e35a8bfd72d921f9606bd39dab14f26456
Jayashan Manodya <kpjmp28@gmail.com>
2026-03-02 16:19:29 +0530

    Create Factory.java

---
+++ BackEnd/src/main/java/online/teaplanter/backend/entity/Factory.java
@@ -0,0 +1,37 @@
+package online.teaplanter.backend.entity;
+
+import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "factories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Factory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true)
+    private String registerNo;
+
+    private String contactNumber;
+
+    private String lorrySupervisorName;
+
+    private String lorrySupervisorContact;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "plantation_id")
+    @JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+    private Plantation plantation;
+}
